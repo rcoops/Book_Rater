c2a63b67a0f41f50cd9c7ac9a2f1cbc1020a3705
rcoops <R.P.Cooper1@edu.salford.ac.uk>
2018-03-30 14:17:06 +0100

    Updated BookRater to work with bootstrap 4

---
+++ web/js/3a41524_part_4.js
@@ -0,0 +1,9 @@
+function filterResults(url) {
+    var filter = document.getElementById('filter').value;
+    window.location.href = Routing.generate(url,
+        {'filter': filter});
+}
+function updateNum(num) {
+    var ratingNum=$('#bookrater_raterbundle_review_ratingNum');
+    ratingNum.val(num);
+}
