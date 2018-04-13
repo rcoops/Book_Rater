d7c7b37fd18f0e23ebe3734a3f8f353450a9de29
rcoops <r.p.cooper1@edu.salford.ac.uk>
2018-04-13 13:28:44 +0100

    fix not found or good reads

--- src/BookRater/RaterBundle/Api/Client/GoodReadsApiClient.php
+++ src/BookRater/RaterBundle/Api/Client/GoodReadsApiClient.php
@@ -5,6 +5,8 @@ namespace BookRater\RaterBundle\Api\Client;
 use BookRater\RaterBundle\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\HttpFoundation\Response;
 
 class GoodReadsApiClient
 {
@@ -39,21 +41,31 @@ class GoodReadsApiClient
         return $data->reviews_widget;
     }
 
+    /**
+     * @param Book $book
+     * @throws GuzzleException
+     */
     public function updateGoodReadsInfo(Book $book)
     {
         $isbn = $book->getIsbn();
 
         if ($isbn) {
             $query = sprintf('review_counts.json?key=%s&isbns=%s', $this->goodBooksDeveloperKey, $isbn);
-            $response = $this->guzzleClient->request('GET', $query);
-            if ($response && $response->getBody()) {
-                $data = json_decode($response->getBody());
-                if (isset($data->books) && !empty($data->books)) {
-                    $reviewInfo = $data->books[0];
-                    $rating = round($reviewInfo->average_rating);
-                    $book->setGoodReadsRating($rating);
-                    $goodReadsId = round($reviewInfo->id);
-                    $book->setGoodReadsId($goodReadsId);
+            try {
+                $response = $this->guzzleClient->request('GET', $query);
+                if ($response && $response->getBody()) {
+                    $data = json_decode($response->getBody());
+                    if (isset($data->books) && !empty($data->books)) {
+                        $reviewInfo = $data->books[0];
+                        $rating = round($reviewInfo->average_rating);
+                        $book->setGoodReadsRating($rating);
+                        $goodReadsId = round($reviewInfo->id);
+                        $book->setGoodReadsId($goodReadsId);
+                    }
+                }
+            } catch (GuzzleException $e) {
+                if ($e->getCode() !== Response::HTTP_NOT_FOUND) {
+                    throw $e; // Not found is just fine as there's no guarantee it will be on the site
                 }
             }
         }
